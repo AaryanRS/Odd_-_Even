65dad206a2e4bd9b28843f5435e94d5dbed24968
AaryanRS <Ramesh.shelar897@gmail.com>
2021-10-18 18:22:37 +0530

    first commit

---
+++ OddorEven.py
@@ -0,0 +1,15 @@
+def oddEven(num) :
+        if (num % 2 == 0) :
+                print("Even")
+        else:
+                print("Odd")
+
+while True:
+        num = int(input("Hi welcome to my project, Please enter the number here = "))
+        oddEven(num)
+
+
+
+
+
+
